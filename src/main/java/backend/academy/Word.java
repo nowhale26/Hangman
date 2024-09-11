68e305f6cdefe20f281c12cedb4e013608f7aa73
nowhale26 <yodap321@gmail.com>
2024-09-11 15:38:15 +0300

    fix: Правки java стиля

--- src/main/java/backend/academy/Word.java
+++ src/main/java/backend/academy/Word.java
@@ -1,16 +1,16 @@
 package backend.academy;
 
 import lombok.Getter;
+
 @Getter
 public class Word {
      private String word;
      private String hint;
      private int difficulty;
 
-    public Word(String word, String hint, int difficulty){
-        this.word=word;
+    public Word(String word, String hint, int difficulty) {
+        this.word = word;
         this.difficulty = difficulty;
         this.hint = hint;
     }
-
 }
